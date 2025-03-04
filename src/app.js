69d1f597400b3abb779792c71afc08e08ba5f4bb
Nikhitha <nikhibeeraka2000@gmail.com>
2025-03-04 20:26:36 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Namaste test");
+app.get("/user",(req,res)=>{
+    res.json({firstName:"Nikhitha",lastName:"Beeraka"});
+})
+app.post("/user",(req,res)=>{
+    console.log("saved data successfully");
+    res.json("saved data to the DB");
+})
+app.get("/userr/:userid/:name/:password",(req,res)=>{
+    console.log(req.params)
+    res.send("goTTTTT itttttt")
+    })
+app.use("/user",(req,res)=>{
+    res.json("HAAAAAAAAAAAAAAAAA")
+    })
+app.delete("/user",(req,res)=>{
+res.json("deleted data successfully");
 })
 app.use("/test",(req,res)=>{
     res.send("Hello world!");
